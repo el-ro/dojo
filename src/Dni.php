e23e56bd356d509b90ebf40895f882629e81dcfc
Rodrigo Munoz <r.munoz2387@gmail.com>
2020-10-03 20:35:20 +0200

    Add some refactor

--- src/Dni.php
+++ src/Dni.php
@@ -10,17 +10,14 @@ use LengthException;
 
 final class Dni
 {
+    private const VALID_LENGTH = 9;
     public function __construct(string $dni)
     {
-        if (\strlen($dni) > 9) {
-            throw new LengthException('Too Long');
-        }
-        if (\strlen($dni) < 9) {
-            throw new LengthException('Too Short');
+        if (\strlen($dni) !== self::VALID_LENGTH) {
+            throw new LengthException(
+                \strlen($dni) > 9 ? 'Too Long' : 'Too Short');
         }
 
         throw new DomainException('Ends with number');
-
     }
-
 }
