a22cdf6ad1b81a32a10602834e856f934494cbc8
Rodrigo Munoz <r.munoz2387@gmail.com>
2020-10-03 20:32:36 +0200

    Add 3rd test

--- src/Dni.php
+++ src/Dni.php
@@ -5,16 +5,22 @@ declare(strict_types=1);
 namespace ElRo\Dojo;
 
 
+use DomainException;
 use LengthException;
 
 final class Dni
 {
     public function __construct(string $dni)
     {
-        if (strlen($dni) > 9) {
+        if (\strlen($dni) > 9) {
             throw new LengthException('Too Long');
         }
-        throw new LengthException('Too Short');
+        if (\strlen($dni) < 9) {
+            throw new LengthException('Too Short');
+        }
+
+        throw new DomainException('Ends with number');
+
     }
 
 }
--- tests/DniTest.php
+++ tests/DniTest.php
@@ -3,23 +3,31 @@ declare(strict_types=1);
 
 namespace Tests\ElRo\Dojo;
 
+use DomainException;
 use ElRo\Dojo\Dni;
 use LengthException;
 use PHPUnit\Framework\TestCase;
 
 final class DniTest extends TestCase
 {
-    public function testShouldFailWhenDniLongerThanMaxLength()
+    public function testShouldFailWhenDniLongerThanMaxLength(): void
     {
         $this->expectException(LengthException::class);
         $this->expectExceptionMessage('Too Long');
         $dni = new Dni('0123456789');
     }
 
-    public function testShouldFailWhenDniShorterThanMinLength()
+    public function testShouldFailWhenDniShorterThanMinLength(): void
     {
         $this->expectException(LengthException::class);
         $this->expectExceptionMessage('Too Short');
         $dni = new Dni('01234567');
+    }
+
+    public function testShouldFailWheDniEndsWithANumber(): void
+    {
+        $this->expectException(DomainException::class);
+        $this->expectExceptionMessage('Ends with number');
+        $dni = new Dni('012345678');
     }
 }
