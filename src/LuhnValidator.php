9e5d23a0e2c3367f63666e9de4af117d38e8429a
Rodrigo Munoz <r.munoz2387@gmail.com>
2020-10-06 23:49:25 +0200

    Kata Luhn validator - first loop

---
+++ src/LuhnValidator.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+
+namespace ElRo\Dojo;
+
+
+final class LuhnValidator
+{
+    public function isValid(string $luhnCode): bool
+    {
+        return true;
+    }
+
+}
---
+++ tests/LuhnValidatorTest.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\ElRo\Dojo;
+
+use ElRo\Dojo\LuhnValidator;
+use PHPUnit\Framework\TestCase;
+
+final class LuhnValidatorTest extends TestCase
+{
+    public function testShouldValidateAllZeros(): void
+    {
+        $validator = new LuhnValidator();
+        $this->assertTrue($validator->isValid('00000000000'));
+    }
+}
